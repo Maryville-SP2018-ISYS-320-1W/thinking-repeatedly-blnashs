5bc30eb4375bfdc507cdff25f3c78959b1ac92fd
Barbara Nash <bnash1@live.maryville.edu>
2018-05-04 11:30:31 -0500

    string 2

--- src/P2_HowManyLines.java
+++ src/P2_HowManyLines.java
@@ -4,13 +4,16 @@
   	Date: 
 */
 
-// 1. Your predicted output 
+// 1. 4
 
 
 
 public class P2_HowManyLines {
 
 	public static void main(String[] args) {
+		for( int i = 1; i <= 3; i++ ) 
+		    System.out.println( "How many lines" );
+		    System.out.println( "are printed?" );
 
 	}
 
@@ -18,4 +21,4 @@ public class P2_HowManyLines {
 
 
 
-// 3. Were you correct? Explain any differences and what you learned.
+// 3. Were you correct? Yes Explain any differences and what you learned.
