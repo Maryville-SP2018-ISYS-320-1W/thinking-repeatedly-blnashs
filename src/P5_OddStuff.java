bb920d4e5ac455fcad3181737b986c4681cd41a6
Barbara Nash <bnash1@live.maryville.edu>
2018-05-04 11:34:46 -0500

    string 3

--- src/P5_OddStuff.java
+++ src/P5_OddStuff.java
@@ -4,18 +4,17 @@
   	Date: 
 */
 
-// 1. Your predicted output 
-
-
+// 1. 4 2 1
 
 public class P5_OddStuff {
-
-	public static void main(String[] args) {
-
-	}
-
+    public static void main( String[] args ) {
+        int number = 4;
+        for( int count = 1; count <= number; count++ ) {
+            System.out.println( number );
+            number = number / 2;
+        }
+    }
 }
-
 
 
 // 3. Were you correct? Explain any differences and what you learned.
